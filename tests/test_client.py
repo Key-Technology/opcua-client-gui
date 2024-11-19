6e6b96fa659a6e1ddfcdd190a7d697e401fec863
Andrew Brown <andrew.brown@miriamtech.com>
2024-11-18 17:41:40 -0800

    move dev dependencies their own file

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,45 +9,48 @@ from PyQt5.QtTest import QTest
 
 from uaclient.mainwindow import Window
 
+URL = "opc.tcp://localhost:48400/freeopcua/server/"
 
-class TestClient:
-    @pytest.fixture
-    def resources(self, qtbot):
-        self.server = Server()
-        url = "opc.tcp://localhost:48400/freeopcua/server/"
-        self.server.set_endpoint(url)
-        self.server.start()
-        self.client = Window()
-        qtbot.addWidget = self.client
-        self.client.ui.addrComboBox.setCurrentText(url)
-        self.client.connect()
-        yield
-        self.client.disconnect()
-        self.server.stop()
+@pytest.fixture
+def server():
+    server = Server()
+    server.set_endpoint(URL)
+    server.start()
+    yield server
+    server.stop()
 
-    def get_attr_value(self, text):
-        idxlist = self.client.attrs_ui.model.match(self.client.attrs_ui.model.index(0, 0), Qt.DisplayRole, text,  1, Qt.MatchExactly | Qt.MatchRecursive)
-        idx = idxlist[0]
-        idx = idx.sibling(idx.row(), 1)
-        item = self.client.attrs_ui.model.itemFromIndex(idx)
-        return item.data(Qt.UserRole).value
+@pytest.fixture
+def client(qtbot, server):
+    client = Window()
+    qtbot.addWidget = client
+    client.ui.addrComboBox.setCurrentText(URL)
+    client.connect()
+    yield client
+    client.disconnect()
 
-    def test_select_objects(self, resources):
-        objects = self.server.nodes.objects
-        self.client.tree_ui.expand_to_node(objects)
-        assert objects == self.client.tree_ui.get_current_node()
-        assert self.client.attrs_ui.model.rowCount() >  6
-        assert self.client.refs_ui.model.rowCount() > 1
+def get_attr_value(text, client):
+    idxlist = client.attrs_ui.model.match(client.attrs_ui.model.index(0, 0), Qt.DisplayRole, text,  1, Qt.MatchExactly | Qt.MatchRecursive)
+    idx = idxlist[0]
+    idx = idx.sibling(idx.row(), 1)
+    item = client.attrs_ui.model.itemFromIndex(idx)
+    return item.data(Qt.UserRole).value
 
-        data = self.get_attr_value("NodeId")
-        assert data == objects.nodeid
+def test_select_objects(client, server):
+    objects = server.nodes.objects
+    client.tree_ui.expand_to_node(objects)
+    assert objects == client.tree_ui.get_current_node()
+    assert client.attrs_ui.model.rowCount() >  6
+    assert client.refs_ui.model.rowCount() > 1
 
-    def test_select_server_node(self, resources):
-        server_node = self.server.nodes.server
-        self.client.tree_ui.expand_to_node(server_node)
-        assert server_node ==  self.client.tree_ui.get_current_node()
-        assert self.client.attrs_ui.model.rowCount() > 6
-        assert self.client.refs_ui.model.rowCount() > 10
+    data = get_attr_value("NodeId", client)
+    assert data == objects.nodeid
 
-        data = self.get_attr_value("NodeId")
-        assert data == server_node.nodeid
+def test_select_server_node(client, server):
+    server_node = server.nodes.server
+    client.tree_ui.expand_to_node(server_node)
+    assert server_node ==  client.tree_ui.get_current_node()
+    assert client.attrs_ui.model.rowCount() > 6
+    assert client.refs_ui.model.rowCount() > 10
+
+    data = get_attr_value("NodeId", client)
+    assert data == server_node.nodeid

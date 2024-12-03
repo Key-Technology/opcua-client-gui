739f9e203da1ae7e2dc729430e05f8b3044ad30b
Andrew Brown <andrew.brown@miriamtech.com>
2024-12-02 16:09:48 -0800

    add graph tests

---
+++ tests/integration/test_graph.py
@@ -0,0 +1,31 @@
+import pytest
+import logging
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMenu
+from asyncua.sync import Server, ua
+from uaclient.mainwindow import Window
+
+URL = "opc.tcp://localhost:48400/freeopcua/server/"
+@pytest.fixture
+def server():
+    server = Server()
+    server.set_endpoint(URL)
+    server.start()
+    yield server
+    server.stop()
+
+
+@pytest.fixture
+def client(qtbot, server):
+    client = Window()
+    qtbot.addWidget = client
+    client.ui.addrComboBox.setCurrentText(URL)
+    client.connect()
+    yield client
+    client.disconnect()
+
+
+def test_restart_timer(qtbot, client, server):
+    client.ui.spinBoxNumberOfPoints.setValue(90)
+    client.ui.buttonApply.click()
+    assert client.graph_ui.N == 90
